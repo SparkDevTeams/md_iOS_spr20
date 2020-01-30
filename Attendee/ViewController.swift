5cb89152f1bfc241fb86f1b093577b73f0a7e9c2
William Fernandez <wfern024@fiu.edu>
2020-01-30 11:51:27 -0500

    Initial Commit

---
+++ Attendee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Attendee
+//
+//  Created by William Fernandez on 1/30/20.
+//  Copyright © 2020 SparkDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
