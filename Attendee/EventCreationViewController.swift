ca9c3901061e02709055394aa135be9e375185b9
William Fernandez <wfern024@fiu.edu>
2020-02-04 13:01:49 -0500

    Event Creation 1

---
+++ Attendee/EventCreationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EventCreationViewController.swift
+//  Attendee
+//
+//  Created by William Fernandez on 1/30/20.
+//  Copyright © 2020 SparkDev. All rights reserved.
+//
+
+import UIKit
+
+class EventCreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
