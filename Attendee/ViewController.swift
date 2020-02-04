f6143e3b6787f7c10f995e1b0a7e758998829e0e
Maia Duschatzky <Maia Duschatzky>
2020-02-04 13:01:55 -0500

    Event Details

--- Attendee/ViewController.swift
+++ Attendee/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var eventEditTitle: UILabel!
+    @IBOutlet weak var eventName: UITextField!
+    @IBOutlet weak var eventLocation: UITextField!
+    @IBOutlet weak var eventDateTime: UITextField!
+    @IBOutlet weak var eventDesc: UITextField!
+    @IBOutlet weak var eventCap: UITextField!
+    @IBOutlet weak var eventAlert: UITextField!
+    @IBOutlet weak var eventURL: UITextField!
+    @IBOutlet weak var eventNotes: UITextField!
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventDetails: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveDetails(_ sender: Any) {
+    }
 }
 
